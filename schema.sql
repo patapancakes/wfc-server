340acaa01aea3e349b83f80f767760f3cc7d6f4f
Pancakes <patapancakes@pagefault.games>
2026-07-06 23:45:27 -0400

    Update schema file for recent changes

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Host:                         localhost
+-- Host:                         127.0.0.1
 -- Server version:               11.8.6-MariaDB-0+deb13u1 from Debian - -- Please help get to 10k stars at https://github.com/MariaDB/Server
 -- Server OS:                    debian-linux-gnu
 -- HeidiSQL Version:             12.18.0.7304
@@ -16,7 +16,7 @@
 
 
 -- Dumping database structure for wfc
-CREATE DATABASE IF NOT EXISTS `wfc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+CREATE DATABASE IF NOT EXISTS `wfc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
 USE `wfc`;
 
 -- Dumping structure for table wfc.events
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS `events` (
   `event_data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`event_data`)),
   `event_time` timestamp NOT NULL DEFAULT utc_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=710 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table wfc.events: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table wfc.gamestats_public_data
 CREATE TABLE IF NOT EXISTS `gamestats_public_data` (
@@ -37,10 +37,11 @@ CREATE TABLE IF NOT EXISTS `gamestats_public_data` (
   `ptype` tinytext NOT NULL,
   `pdata` text NOT NULL,
   `modified_time` timestamp NOT NULL,
-  KEY `one_pdata_constraint` (`profile_id`,`dindex`(255),`ptype`(255)) USING BTREE
+  KEY `one_pdata_constraint` (`profile_id`,`dindex`(255),`ptype`(255)) USING BTREE,
+  CONSTRAINT `FK_gamestats_public_data_profiles` FOREIGN KEY (`profile_id`) REFERENCES `profiles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table wfc.gamestats_public_data: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table wfc.mario_kart_wii_sake
 CREATE TABLE IF NOT EXISTS `mario_kart_wii_sake` (
@@ -54,30 +55,16 @@ CREATE TABLE IF NOT EXISTS `mario_kart_wii_sake` (
   `upload_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `one_time_per_course_constraint` (`courseid`,`pid`),
+  KEY `FK_mario_kart_wii_sake_profiles` (`pid`),
+  CONSTRAINT `FK_mario_kart_wii_sake_profiles` FOREIGN KEY (`pid`) REFERENCES `profiles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `mario_kart_wii_sake_regionid_check` CHECK (`regionid` >= 1 and `regionid` <= 7),
   CONSTRAINT `mario_kart_wii_sake_courseid_check` CHECK (`courseid` >= 0 and `courseid` <= 32767),
   CONSTRAINT `mario_kart_wii_sake_score_check` CHECK (`score` > 0 and `score` < 360000),
-  CONSTRAINT `mario_kart_wii_sake_pid_check` CHECK (`pid` > 0),
   CONSTRAINT `mario_kart_wii_sake_playerinfo_check` CHECK (octet_length(`playerinfo`) = 108),
   CONSTRAINT `mario_kart_wii_sake_ghost_check` CHECK (`ghost` is null or octet_length(`ghost`) >= 148 and octet_length(`ghost`) <= 10240)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table wfc.mario_kart_wii_sake: ~0 rows (approximately)
-
--- Dumping structure for table wfc.sake_records
-CREATE TABLE IF NOT EXISTS `sake_records` (
-  `game_id` int(10) unsigned NOT NULL,
-  `table_id` tinytext NOT NULL,
-  `record_id` int(10) unsigned NOT NULL DEFAULT rand(2147483647),
-  `owner_id` int(10) unsigned NOT NULL,
-  `fields` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`fields`)),
-  `create_time` timestamp NOT NULL DEFAULT utc_timestamp(),
-  `update_time` timestamp NOT NULL DEFAULT utc_timestamp(),
-  UNIQUE KEY `one_sake_record_constraint` (`game_id`,`table_id`,`record_id`) USING HASH,
-  CONSTRAINT `sake_records_fields_check` CHECK (json_length(json_keys(`fields`)) <= 64)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table wfc.sake_records: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table wfc.profiles
 CREATE TABLE IF NOT EXISTS `profiles` (
@@ -95,10 +82,43 @@ CREATE TABLE IF NOT EXISTS `profiles` (
   `ban_reason_hidden` tinytext DEFAULT NULL,
   `ban_moderator` tinytext DEFAULT NULL,
   `ban_tos` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `FK_profiles_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table wfc.profiles: ~0 rows (approximately)
+-- Data exporting was unselected.
+
+-- Dumping structure for table wfc.sake_records
+CREATE TABLE IF NOT EXISTS `sake_records` (
+  `game_id` int(10) unsigned NOT NULL,
+  `table_id` tinytext NOT NULL,
+  `record_id` int(10) unsigned NOT NULL DEFAULT rand(2147483647),
+  `owner_id` int(10) unsigned NOT NULL,
+  `fields` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`fields`)),
+  `create_time` timestamp NOT NULL DEFAULT utc_timestamp(),
+  `update_time` timestamp NOT NULL DEFAULT utc_timestamp(),
+  UNIQUE KEY `one_sake_record_constraint` (`game_id`,`table_id`,`record_id`) USING HASH,
+  KEY `FK_sake_records_profiles` (`owner_id`),
+  CONSTRAINT `FK_sake_records_profiles` FOREIGN KEY (`owner_id`) REFERENCES `profiles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sake_records_fields_check` CHECK (json_length(json_keys(`fields`)) <= 64)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table wfc.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL,
+  `unitcd` tinyint(1) unsigned NOT NULL,
+  `macadr` char(12) NOT NULL,
+  `passwd` smallint(6) DEFAULT NULL COMMENT 'ds only',
+  `csnum` char(11) DEFAULT NULL COMMENT 'wii only',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `macadr` (`macadr`),
+  UNIQUE KEY `csnum` (`csnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
